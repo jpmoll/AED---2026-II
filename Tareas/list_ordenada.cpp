6d5459c5d5fa31ff967a59e8fd590c5ec9bd167e
jpmoll <jpmollapaza@ucsp.edu.pe>
2026-08-28 12:28:32 -0500

    Update list_ordenada.cpp

--- Tareas/list_ordenada.cpp
+++ Tareas/list_ordenada.cpp
@@ -1,41 +1,40 @@
-
 #include <iostream>
 using namespace std;
 
 template <class T>
 struct ASC {
-    bool operator ()(T a, T b){
+    bool operator()(T a, T b) {
         return a < b;
     }
 };
 
 template <class T>
 struct DES {
-    bool operator ()(T a, T b){
+    bool operator()(T a, T b) {
         return a > b;
     }
 };
 
 template <class T, class O, int N = 10>
-class list_ord{
+class list_ord {
 private:
     int tam = N;
     int elem = 0;
     O ord;
     T Arr[N];
 public:
-    
-    bool find (T val, T* &pos){
-        
+
+    bool find(T val, T* &pos) {
         T* ini = Arr;
         T* end = Arr + elem - 1;
-        
-        while(ini <= end){
-            T* mid = Arr + ((end - ini) / 2);
-            if (*mid == val){
+
+        while (ini <= end) {
+            T* mid = ini + (end - ini) / 2;
+
+            if (*mid == val) {
                 pos = mid;
                 return true;
-            } else if (*mid < val){
+            } else if (ord(*mid, val)) {
                 ini = mid + 1;
             } else {
                 end = mid - 1;
@@ -44,38 +43,72 @@ public:
         pos = ini;
         return false;
     }
-    
-    void add (T e){
-        T* pos;
-        bool encontrado = find(e, pos);
-        if (elem < tam){
-            *(Arr + elem) = e;
-            elem++;
-        } else {
+
+    void add(T e) {
+        if (elem >= tam) {
             cout << "Lista llena" << endl;
+            return;
         }
-    };
-    
-    bool del (T e){
-        // comprobar si solo tiene un valor
-        // la busqueda binaria sirve con más de 2 valores
-        
-        return false;
-    };
-    void print (){
-        for(T* p = Arr; p < Arr + elem; p++){
+
+        T* pos;
+        find(e, pos);
+
+        for (T* p = Arr + elem; p > pos; p--) {
+            *p = *(p - 1);
+        }
+
+        *pos = e;
+        elem++;
+    }
+
+    bool del(T e) {
+    T* pos;
+    if (!find(e, pos)) {
+        return false;   // no existe, nada que borrar
+    }
+
+    // expandir hacia la izquierda mientras siga siendo igual a e
+    T* izq = pos;
+    while (izq > Arr && *(izq - 1) == e) {
+        izq--;
+    }
+
+    // expandir hacia la derecha mientras siga siendo igual a e
+    T* der = pos;
+    while (der < Arr + elem - 1 && *(der + 1) == e) {
+        der++;
+    }
+
+    int cantidad = der - izq + 1;   // cuántos elementos iguales hay
+
+    // desplazar todo lo que está después de 'der' hacia 'izq'
+    for (T* p = der + 1; p < Arr + elem; p++) {
+        *(p - cantidad) = *p;
+    }
+
+    elem -= cantidad;
+    return true;
+}
+
+
+    void print() {
+        for (T* p = Arr; p < Arr + elem; p++) {
             cout << *p << ", ";
         }
         cout << endl;
-    };
+    }
 };
-
-
 
 int main() {
     list_ord<int, ASC<int>> lo;
     lo.add(5);
     lo.add(3);
     lo.add(2);
-    lo.print();
+    lo.print();   // 2, 3, 5
+
+    list_ord<int, DES<int>> lo2;
+    lo2.add(5);
+    lo2.add(3);
+    lo2.add(2);
+    lo2.print();  // 5, 3, 2
 }
