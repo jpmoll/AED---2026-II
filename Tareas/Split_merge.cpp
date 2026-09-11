740076b6c557e5b57b69ada67d3234d9c76879f9
jpmoll <jpmollapaza@ucsp.edu.pe>
2026-09-11 14:20:55 -0500

    Update Split_merge.cpp

--- Tareas/Split_merge.cpp
+++ Tareas/Split_merge.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
 template <class C>
 struct UTIL {
+    
     
     int* split(int* ini, int* fin) {
         
@@ -15,13 +15,13 @@ struct UTIL {
         while (q != fin) {
             if (op(*q)) {
                 swap(*p, *q);
-                ++p;
+                p++;
             }
-            ++q;
+            q++;
         }
-        
         return p;
     }
+    
     
     void merge(int* ini, int* mid, int* fin) {
         
@@ -33,22 +33,22 @@ struct UTIL {
             if (*p <= *q) {
                 ++p;
             }
+            
             else {
                 
                 int temp = *q;
-                
                 int* r = q;
                 
                 while (r != p) {
                     *r = *(r - 1);
-                    --r;
+                    r++;
                 }
                 
                 *p = temp;
                 
-                ++p;
-                ++q;
-                ++mid;
+                p++;
+                q++;
+                mid++;
             }
         }
     }
@@ -76,24 +76,26 @@ void print(int* ini, int* fin) {
 
 int main() {
 
-    int arr1[] = { 1,4,8,7,21,3,4,5,6,9 };
+    int arr1[] = {1,4,8,7,21,3,4,5,6,9};
+    int arr2[] = {3,6,9,33,1,2,4,8,11,23,30,60};
+    
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
 
     UTIL<div3> util;
     
-    /*
+    
     // Split
     print(arr1, arr1 + n1);
     util.split(arr1, arr1 + n1);
     print(arr1, arr1 + n1);
     
     cout << endl;
-    */
-     
+    
     // Merge
-    print(arr1, arr1 + n1);
-    util.merge(arr1, arr1 + 5, arr1 + n1);
-    print(arr1, arr1 + n1);
+    print(arr2, arr2 + n2);
+    util.merge(arr2, arr2 + 4, arr2 + n1);
+    print(arr2, arr2 + n2);
 
     return 0;
 }
