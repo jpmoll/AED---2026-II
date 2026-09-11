02166b93d529db2804a7e63cc496c462b507fa9d
jpmoll <jpmollapaza@ucsp.edu.pe>
2026-09-11 14:02:27 -0500

    Update list_ordenada.cpp

--- Tareas/list_ordenada.cpp
+++ Tareas/list_ordenada.cpp
@@ -18,33 +18,45 @@ struct DES {
 template <class T, class O, int N = 10>
 class list_ord {
 private:
+    
     int tam = N;
     int elem = 0;
     O ord;
     T Arr[N];
+    
 public:
 
     bool find(T val, T* &pos) {
+        
         T* ini = Arr;
         T* end = Arr + elem - 1;
 
         while (ini <= end) {
-            T* mid = ini + (end - ini) / 2;
+            
+            T* mid = ini + (end-ini)/2;
 
             if (*mid == val) {
                 pos = mid;
                 return true;
+                
             } else if (ord(*mid, val)) {
+                
                 ini = mid + 1;
+                
             } else {
+                
                 end = mid - 1;
+                
             }
         }
         pos = ini;
         return false;
     }
+    
+    
 
     void add(T e) {
+        
         if (elem >= tam) {
             cout << "Lista llena" << endl;
             return;
@@ -60,33 +72,37 @@ public:
         *pos = e;
         elem++;
     }
+    
+    
 
     bool del(T e) {
     T* pos;
+    
     if (!find(e, pos)) {
-        return false;   // no existe, nada que borrar
+        return false;
     }
 
-    // expandir hacia la izquierda mientras siga siendo igual a e
+    // expandir
+    // izquierda
     T* izq = pos;
     while (izq > Arr && *(izq - 1) == e) {
         izq--;
     }
 
-    // expandir hacia la derecha mientras siga siendo igual a e
+    // derecha
     T* der = pos;
     while (der < Arr + elem - 1 && *(der + 1) == e) {
         der++;
     }
 
-    int cantidad = der - izq + 1;   // cuántos elementos iguales hay
+    int num_iguales = der - izq + 1;
 
-    // desplazar todo lo que está después de 'der' hacia 'izq'
+    // desplazamiento der a izq
     for (T* p = der + 1; p < Arr + elem; p++) {
-        *(p - cantidad) = *p;
+        *(p - num_iguales) = *p;
     }
 
-    elem -= cantidad;
+    elem -= num_iguales;
     return true;
 }
 
@@ -104,11 +120,14 @@ int main() {
     lo.add(5);
     lo.add(3);
     lo.add(2);
-    lo.print();   // 2, 3, 5
+    lo.add(6);
+    lo.add(9);
+    lo.add(1);
+    lo.print();
 
     list_ord<int, DES<int>> lo2;
     lo2.add(5);
     lo2.add(3);
     lo2.add(2);
-    lo2.print();  // 5, 3, 2
+    lo2.print();
 }
