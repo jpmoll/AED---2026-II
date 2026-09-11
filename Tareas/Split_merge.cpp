36135d89787e1c1c4c01f9d6a3bbaf363fc91eb9
jpmoll <jpmollapaza@ucsp.edu.pe>
2026-09-11 13:12:18 -0500

    Update Split_merge.cpp

--- Tareas/Split_merge.cpp
+++ Tareas/Split_merge.cpp
@@ -1,56 +1,99 @@
-
 #include <iostream>
 using namespace std;
 
-struct UTIL {
-    void split(int* ini, int* fin);
-    void merge(int* ini, int* mid, int* fin);
-};
 
-class div3 {
-    bool operator()(int a) {
-        return !(a % 3);
+template <class C>
+struct UTIL {
+    
+    int* split(int* ini, int* fin) {
+        
+        C op;
+        
+        int* p = ini;
+        int* q = ini;
+        
+        while (q != fin) {
+            if (op(*q)) {
+                swap(*p, *q);
+                ++p;
+            }
+            ++q;
+        }
+        
+        return p;
+    }
+    
+    void merge(int* ini, int* mid, int* fin) {
+        
+        int* p = ini;
+        int* q = mid;
+        
+        while (p != q && q != fin) {
+            
+            if (*p <= *q) {
+                ++p;
+            }
+            else {
+                
+                int temp = *q;
+                
+                int* r = q;
+                
+                while (r != p) {
+                    *r = *(r - 1);
+                    --r;
+                }
+                
+                *p = temp;
+                
+                ++p;
+                ++q;
+                ++mid;
+            }
+        }
     }
 };
 
-int main()
-{
-    int arr1[] = { 1,4,8,7,21,3 ,4,5,6,9 };
-    int arr2[] = { 3,2,1,5,9,7,11,8,33,25,4 };
 
-    //  3, 9, 33,  2, 1, 5, 7, 11, 8, 25, 4
-    // - cumple -   ------ no cumple ------
+class div3 {
+public:
 
-    //  1,4,8,7,21,3,4,5,6,9
-    //  ^          ^       ^
-    // ini        mid     fin
-    // 
-    //  1,4,8,7,21,3     p > q - Falso  p++
-    //  p          q
-    // 
-    //  1,4,8,7,21,3     p > q - True   Swap
-    //    p        q
-    // 
-    //      r
-    //  1,3,8,7,21,4
-    //    p        q
-    // 
-    //    
-    //  1,3,4,8,7,21
-    //      p      q
-    // 
-    //  1,3,4,8,7,21     q++
-    //            p=q
-    // 
-    //  1,3,4,8,7,21,4
-    //  p            q 
-    // 
-    // 
-    //    r ->           r recorre los elementos a la derecha despues de un swap 
-    //  1,3,4,8,7,21
-    //  p ->      q->    p avanza hasta llegar a q,  q avanza hasta llegar a fin (f)
-    //
+    bool operator()(int a) {
+        return !(a % 3);
+    }
+
+};
 
 
-    cout << "Hello World 123!\n";
+void print(int* ini, int* fin) {
+
+    for (int* it = ini; it != fin; ++it)
+        cout << *it << " ";
+
+    cout << endl;
+}
+
+
+int main() {
+
+    int arr1[] = { 1,4,8,7,21,3,4,5,6,9 };
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+
+    UTIL<div3> util;
+    
+    /*
+    // Split
+    print(arr1, arr1 + n1);
+    util.split(arr1, arr1 + n1);
+    print(arr1, arr1 + n1);
+    
+    cout << endl;
+    */
+     
+    // Merge
+    print(arr1, arr1 + n1);
+    util.merge(arr1, arr1 + 5, arr1 + n1);
+    print(arr1, arr1 + n1);
+
+    return 0;
 }
